b4fd81055f850c2c3f1c9cff96d6bc10d37b2ca9
RajNara <84680020+RajNara@users.noreply.github.com>
2021-09-08 17:48:38 -0500

    Add files via upload
    Coded a simple outline of what the website will look like, with 2 game boards and a button to start the game.

---
+++ eecs448project1/script.js
@@ -0,0 +1,15 @@
+let canvas;
+
+var c = document.getElementById("board1");
+var ctx = c.getContext("2d");
+
+
+
+
+
+
+function start()
+{
+    document.getElementById('hidden-div').classList.add('show');
+    document.getElementById('button-id').classList.add('hide');
+}
