bba5bab45d5252ce276283e364e660b0ac1b2d24
RajNara <rajnara2001@gmail.com>
2021-09-11 19:22:31 -0500

    Backbone code of the website + grid
    Added color and buttons to specify the number of ships the player was choosing, and added the grids as well.

--- eecs448project1/script.js
+++ eecs448project1/script.js
@@ -1,15 +1,36 @@
 let canvas;
 
-var c = document.getElementById("board1");
-var ctx = c.getContext("2d");
+var c1 = document.getElementById("board1");
+var ctx1 = c1.getContext("2d");
+var c2 = document.getElementById("board2");
+var ctx2 = c2.getContext("2d");
+ctx1.strokeStyle = 'red';
+ctx2.strokeStyle = 'blue';
 
-
-
-
-
-
-function start()
+function drawgrid()
 {
-    document.getElementById('hidden-div').classList.add('show');
-    document.getElementById('button-id').classList.add('hide');
+    for (let i = 1; i < 10; i++)
+    {
+        ctx1.beginPath();
+        ctx1.moveTo(i * 50, 0)
+        ctx1.lineTo(i * 50, 450)
+        ctx1.stroke();
+
+        ctx1.beginPath();
+        ctx1.moveTo(0, i * 45)
+        ctx1.lineTo(450, i * 45)
+        ctx1.stroke();
+
+        ctx2.beginPath();
+        ctx2.moveTo(i * 50, 0)
+        ctx2.lineTo(i * 50, 450)
+        ctx2.stroke();
+
+        ctx2.beginPath();
+        ctx2.moveTo(0, i * 45)
+        ctx2.lineTo(450, i * 45)
+        ctx2.stroke();
+    }
+
+    document.getElementById('start').disabled = 'disabled';
 }
