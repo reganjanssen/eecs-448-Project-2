c72f7a369b5d5eb7fd0dfefcc7caba79c6d088d0
zachnkck <90000333+zachnkck@users.noreply.github.com>
2021-09-18 07:03:19 -0500

    Added doc folder, placement proof-of-concept, ship arrays, function that determines if game is over

--- eecs448project1/script.js
+++ eecs448project1/script.js
@@ -8,20 +8,27 @@ let p1Grid;
 let p1Guess;
 let p2Grid;
 let p2Guess;
+var gameType;	//set equal to number of ships on the board
 
-let p1GridArr = createArray(10, 9);
+var p1Ships = [];
+var p2Ships = [];
+
+var p1GridArr = createArray(10, 9);					//Note: could save time later by saving location of ship as its size - 1x1 ship at (X, Y) means value at (X, Y) = 1
 let p1GuessArr = createArray(10, 9);
-let p2GridArr = createArray(10, 9);
+var p2GridArr = createArray(10, 9);
 let p2GuessArr = createArray(10, 9);
 
 class Ship
 {
     sunk = false; // Our ship obviously won't start already sunk.
-    constructor(size, posX, posY)
+    constructor(size, posX, posY, facing)
     {
         this.size = size; // Carriers are 5, battleships are 4, cruisers & subs are 3, destroyers are 2
         this.posX = posX;
         this.posY = posY;
+		this.facing = facing;	// posX and posY determine "origin" of ship, facing determines where extra ship parts go
+								//0 = Up, 1 = Right, 2 = Down, 3 = Left ------ ex. 1x2 ship with origin E6 and facing 1 will occupy E6 AND E7
+								//Exception: 1x1 ship given default facing of "0"
     }
 
     sink()
@@ -43,6 +50,14 @@ class Ship
     {
         return this.sunk;
     }
+	
+	getFacing() {
+		return this.facing;
+	}
+	
+	getSize() {
+		return this.size;
+	}
 }
 
 function createArray(length)
@@ -104,52 +119,132 @@ function playBoard(rows, cols, classname, callback) // The "callback" is a funct
     return grid;
 }
 
-function placeShips(arr)
-{
-    var ships = 0;
-    while(ships < 5)
-    {
-        let row = Math.floor(Math.random() * 10);
-        let col = Math.floor(Math.random() * 9);
-        let vertOrHoriz = Math.floor(Math.random() * 2);
-        
-        if(vertOrHoriz == 0) // Vertical is chosen
-        {
-            if((row < 7 && arr[row][col] == null && arr[row + 1][col] == null && arr[row + 2][col] == null && arr[row + 3][col] == null)) // Check if the appropriate spaces are empty
-            {
-                arr[row][col] = 1;
-                arr[row + 1][col] = 1;
-                arr[row + 2][col] = 1;
-                ships++;
-            }
-            else // If not, cut the loop and try again
-            {
-                continue;
-            }
-        }
-        if(vertOrHoriz == 1) // Horizontal is chosen
-        {
-            if((col < 3 && arr[row][col] == null && arr[row][col + 1] == null && arr[row][col + 2] == null && arr[row][col + 3] == null)) // Check if the adjacent spaces are empty
-            {
-                console.log("Placed one - horizontal at " + row + ", " + col);
-                arr[row][col] = 1;
-                arr[row][col + 1] = 1;
-                arr[row][col + 2] = 1;
-                ships++;
-            }
-            else // If not, cut the loop and try again
-            {
-                continue;
-            }
-        }
-    }
-    return arr;
+//Following placeShipx function are definitely not optimal, but should work
+//Need to allow placement by clicking on grid
+//CURRENTLY EXPECTS PERFECT INPUT (LETTER+NUMBER BOTH WITHIN GRID)
+function getCoords() {
+	let coordString = window.prompt("Enter Grid ID (ex. B3)");
+	coordSplit = coordString.split("");
+	let coordX = parseInt(coordSplit[1]) - 1;		//Both coords normalized to grid format (begins with 0) (goes row, column)
+	let coordY = parseInt(coordSplit[0], 36) - 10;
+	
+	let coordTogether = [coordY, coordX];
+	
+	return(coordTogether);
 }
+
+function placeShip1(player) {
+	alert("Placing 1x1 ship");
+	let isSafePlace = 0;
+	
+	do {
+		wantedCoords = getCoords().slice(0);
+	
+		//check if space is empty
+		if((player == 1) && (p1GridArr[wantedCoords[0]][wantedCoords[1]] == null)) {
+			isSafePlace = 1;
+			p1Ships[0] = new Ship(1, wantedCoords[1], wantedCoords[0], 0);
+			p1GridArr[wantedCoords[0]][wantedCoords[1]] = 1;
+			
+			console.log("Ship 1 created for Player 1: Size " + p1Ships[0].getSize() + ", Row: " + p1Ships[0].getPosY() + ", Column: " + p1Ships[0].getPosX() + ", Facing: " + p1Ships[0].getFacing());
+			console.log("Synced with p1GridArr?: ");
+			console.log(p1GridArr[p1Ships[0].getPosY()][p1Ships[0].getPosX()] == 1);
+		}
+		else if((player == 2) && (p2GridArr[wantedCoords[0]][wantedCoords[1]] == null)) {
+			isSafePlace = 1;
+			p2Ships[0] = new Ship(1, wantedCoords[1], wantedCoords[0], 0);
+			p2GridArr[wantedCoords[0]][wantedCoords[1]] = 1;
+			
+			console.log("Ship 1 created for Player 2: Size " + p2Ships[0].getSize() + ", Row: " + p2Ships[0].getPosY() + ", Column: " + p2Ships[0].getPosX() + ", Facing: " + p2Ships[0].getFacing());
+			console.log("Synced with p2GridArr?: ");
+			console.log(p2GridArr[p2Ships[0].getPosY()][p2Ships[0].getPosX()] == 1); 
+		}
+	
+	} while(isSafePlace == 0);
+	
+}
+
+function placeShip2(player) {
+	alert("Placing 1x2 ship");
+	//Same as above, except: needs facing restrictions
+	//ex. facing is 0, check (length of ship - 1) tiles above the origin
+}
+
+function placeShip3(player) {
+	alert("Placing 1x3 ship");	
+	
+}
+
+function placeShip4(player) {
+	alert("Placing 1x4 ship");	
+	
+}
+
+function placeShip5(player) {
+	alert("Placing 1x5 ship");	
+	
+}
+
+function placeShip6(player) {
+	alert("Placing 1x6 ship");	
+	
+}
+
+//TODO: Get "gameType" via "x ships per person" buttons before calling this
+//REMINDER: REMOVE "gameType" VALUE DECLARED HERE - FOR TESTING ONLY
+function placeShips(player)
+{
+	gameType = 1;
+	
+	switch(gameType) {
+		case 1:
+		placeShip1(player);
+		
+		break;
+		case 2:
+		placeShip1(player);
+		placeShip2(player);
+		
+		break;
+		case 3:
+		placeShip1(player);
+		placeShip2(player);
+		placeShip3(player);
+		
+		break;
+		case 4:
+		placeShip1(player);
+		placeShip2(player);
+		placeShip3(player);
+		placeShip4(player);		
+		
+		break;
+		case 5:
+		placeShip1(player);
+		placeShip2(player);
+		placeShip3(player);
+		placeShip4(player);	
+		placeShip5(player);
+		
+		break;
+		case 6:
+		placeShip1(player);
+		placeShip2(player);
+		placeShip3(player);
+		placeShip4(player);
+		placeShip5(player);
+		placeShip6(player);
+		
+		break;
+	}
+}
+
 
 function drawgrid()
 {
-    placeShips(p1GridArr);
-    placeShips(p2GridArr);
+    placeShips(1);
+    placeShips(2);
+	
     document.getElementById('start').disabled = 'disabled';
     document.getElementById("playerNum").innerHTML = curPlyr;
 
@@ -187,7 +282,7 @@ function drawgrid()
 
     p1Grid = document.getElementById("boards").appendChild(player1grid);
     p1Grid.setAttribute("id", "p1Grid");
-    p1GridArr = createArray(10, 9);
+	p1GridArr = createArray(10, 9);
 
     p2Guess = document.getElementById("guessBoards").appendChild(player2guess);
     p2Guess.setAttribute("id", "p2Guess");
@@ -208,6 +303,8 @@ function ship1()
     document.getElementById('4ship').disabled = true;
     document.getElementById('5ship').disabled = true;
     document.getElementById('6ship').disabled = true;
+	
+	gameType = 1;
 
     alert("You and your opponent have the following ships: 1x1");
 }
@@ -219,6 +316,8 @@ function ship2()
     document.getElementById('4ship').disabled = true;
     document.getElementById('5ship').disabled = true;
     document.getElementById('6ship').disabled = true;
+	
+	gameType = 2;
 
     alert("You and your opponent have the following ships: 1x1, 1x2");
 
@@ -232,6 +331,8 @@ function ship3()
     document.getElementById('5ship').disabled = true;
     document.getElementById('6ship').disabled = true;
 
+	gameType = 3;
+
     alert("You and your opponent have the following ships: 1x1, 1x2, 1x3");
 
 }
@@ -243,6 +344,8 @@ function ship4()
     document.getElementById('3ship').disabled = true;
     document.getElementById('5ship').disabled = true;
     document.getElementById('6ship').disabled = true;
+	
+	gameType = 4;
 
     alert("You and your opponent have the following ships: 1x1, 1x2, 1x3, 1x4");
 
@@ -255,6 +358,8 @@ function ship5()
     document.getElementById('3ship').disabled = true;
     document.getElementById('4ship').disabled = true;
     document.getElementById('6ship').disabled = true;
+	
+	gameType = 5;
 
     alert("You and your opponent have the following ships: 1x1, 1x2, 1x3, 1x4, 1x5");
 
@@ -267,10 +372,40 @@ function ship6()
     document.getElementById('3ship').disabled = true;
     document.getElementById('4ship').disabled = true;
     document.getElementById('5ship').disabled = true;
+	
+	gameType = 6;
 
     alert("You and your opponent have the following ships: 1x1, 1x2, 1x3, 1x4, 1x5, 1x6");
 
 }
+
+//Function should be run after curPlyr has sunk an opposing ship but before the turn changes
+//Returns 0 if the game continues, or the ID of the winner (1 or 2);
+function getWinner() {
+	let nonSunk = false;
+	if(curPlyr == 1) {
+		for(let i = 0; i < gameType; i++) {
+			if(p2Ships[i].isSunk() == false) {
+				nonSunk = true;
+			}
+		}
+	}
+	else {
+		for(let i = 0; i < gameType; i++) {
+			if(p1Ships[i].isSunk() == false) {
+				nonSunk = true;
+			}
+		}
+	}
+	if(nonSunk == false) {
+		return(curPlyr);
+	}
+	else {
+		return(0);
+	}
+}
+				
+		
 
 //This function will block the board while the players are changing in order to prevent the a player from seeing the others ship locations.
 //The button must be pressed twice: Once to block the boards so the players can switch, and then once more to switch to the other player.
@@ -304,7 +439,7 @@ function changeTurn()
 }
 
 function gameLoop()
-{
+{		
     while(gameOn)
     {
         p1Grid.style.display = "inline";
