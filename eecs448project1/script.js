f5089dffea9095c748c5ef7eefa024a2e5486a3d
January <me@drewf.ink>
2021-09-17 19:05:43 -0500

    Randomly place ships on game start

--- eecs448project1/script.js
+++ eecs448project1/script.js
@@ -4,10 +4,15 @@ const col = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 var isChanging = 0;
 var curPlyr = 1;
 var gameOn = false;
-let p1grid;
-let p1guess;
-let p2grid;
-let p2guess;
+let p1Grid;
+let p1Guess;
+let p2Grid;
+let p2Guess;
+
+let p1GridArr = createArray(10, 9);
+let p1GuessArr = createArray(10, 9);
+let p2GridArr = createArray(10, 9);
+let p2GuessArr = createArray(10, 9);
 
 class Ship
 {
@@ -40,11 +45,25 @@ class Ship
     }
 }
 
+function createArray(length)
+{
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}
+
 function playBoard(rows, cols, classname, callback) // The "callback" is a function we use for our event listener.
 {
     var letter = 65; // 65 is ASCII for the letter A. We use this when numbering the grid.
     var i = 1; // This is also used for numbering the grid.
     var grid = document.createElement('table');
+
     grid.className = classname; // Determines if it's the 1st or 2nd player grid
     for (var r = 0; r < rows; ++r)
     {
@@ -53,13 +72,31 @@ function playBoard(rows, cols, classname, callback) // The "callback" is a funct
         {
             var cell = tr.appendChild(document.createElement('td')); // Put a new column in
             cell.innerHTML = `${String.fromCharCode(letter)}${i++}`; // Insert the grid number, like A1, B2, C3 etc.
-            cell.addEventListener('click', (function(element, r, c, i) // This inserts a "listener" for the event so that we know when it's clicked.
+            if(grid.className == "p1-grid")
             {
-                return function()
+                if(p1GridArr[r][c] == 1)
                 {
-                    callback(element, r, c, i); // Pass the element, rows, columns, and item number back.
+                    cell.className = 'clicked';
                 }
-            })(cell, r, c, i), false);
+            }
+            if(grid.className == "p2-grid")
+            {
+                if(p2GridArr[r][c] == 1)
+                {
+                    cell.className = 'clicked';
+                }
+            }
+            else
+            {
+                cell.addEventListener('click', (function(element, r, c, i) // This inserts a "listener" for the event so that we know when it's clicked.
+                {
+                    return function()
+                    {
+                        callback(element, r, c, i); // Pass the element, rows, columns, and item number back.
+                    }
+                })(cell, r, c, i), false);
+            }
+            
         }
         i = 1;
         letter++;
@@ -67,54 +104,99 @@ function playBoard(rows, cols, classname, callback) // The "callback" is a funct
     return grid;
 }
 
+function placeShips(arr)
+{
+    var ships = 0;
+    while(ships < 5)
+    {
+        let row = Math.floor(Math.random() * 10);
+        let col = Math.floor(Math.random() * 9);
+        let vertOrHoriz = Math.floor(Math.random() * 2);
+        
+        if(vertOrHoriz == 0) // Horizontal is chosen
+        {
+            if((row < 7 && arr[row][col] == null && arr[row + 1][col] == null && arr[row + 2][col] == null && arr[row + 3][col] == null)) // Check if the appropriate spaces are empty
+            {
+                console.log("Placed one - vertical at " + row + ", " + col);
+                arr[row][col] = 1;
+                arr[row + 1][col] = 1;
+                arr[row + 2][col] = 1;
+                ships++;
+            }
+            else // If not, cut the loop and try again
+            {
+                continue;
+            }
+        }
+        if(vertOrHoriz == 1) // Vertical is chosen
+        {
+            if((col < 3 && arr[row][col] == null && arr[row][col + 1] == null && arr[row][col + 2] == null && arr[row][col + 3] == null)) // Check if the adjacent spaces are empty
+            {
+                console.log("Placed one - horizontal at " + row + ", " + col);
+                arr[row][col] = 1;
+                arr[row][col + 1] = 1;
+                arr[row][col + 2] = 1;
+                ships++;
+            }
+            else // If not, cut the loop and try again
+            {
+                continue;
+            }
+        }
+    }
+    return arr;
+}
+
 function drawgrid()
 {
+    placeShips(p1GridArr);
+    placeShips(p2GridArr);
     document.getElementById('start').disabled = 'disabled';
     document.getElementById("playerNum").innerHTML = curPlyr;
 
     var player1grid = playBoard(10, 9, "p1-grid", function(cell, row, col, i)
     {
-        console.log("x: "+ row);
-        console.log("y: " + col);
+        console.log("x: " + row + " y: " + col + " i: " + i);
 
         cell.className = 'clicked';
     });
 
     var player2grid = playBoard(10, 9, "p2-grid", function(cell, row, col, i)
     {
-        console.log("x: "+ row);
-        console.log("y: " + col);
+        console.log("x: " + row + " y: " + col);
 
         cell.className = 'clicked';
     });
 
-    var player1guess = playBoard(10, 9, "p1-grid", function(cell, row, col, i)
+    var player1guess = playBoard(10, 9, "p1-guess", function(cell, row, col, i)
     {
-        console.log("x: "+ row);
-        console.log("y: " + col);
+        console.log("x: " + row + " y: " + col);
 
         cell.className = 'clicked';
     });
 
-    var player2guess = playBoard(10, 9, "p2-grid", function(cell, row, col, i)
+    var player2guess = playBoard(10, 9, "p2-guess", function(cell, row, col, i)
     {
-        console.log("x: "+ row);
-        console.log("y: " + col);
+        console.log("x: " + row + " y: " + col);
 
         cell.className = 'clicked';
     });
 
-    p1guess = document.getElementById("guessBoards").appendChild(player1guess);
-    p1guess.setAttribute("id", "p1guess");
+    p1Guess = document.getElementById("guessBoards").appendChild(player1guess);
+    p1Guess.setAttribute("id", "p1Guess");
+    p1GuessArr = createArray(10, 9);
 
-    p1grid = document.getElementById("boards").appendChild(player1grid);
-    p1grid.setAttribute("id", "p1grid");
+    p1Grid = document.getElementById("boards").appendChild(player1grid);
+    p1Grid.setAttribute("id", "p1Grid");
+    p1GridArr = createArray(10, 9);
 
-    p2guess = document.getElementById("guessBoards").appendChild(player2guess);
-    p2guess.setAttribute("id", "p2guess");
+    p2Guess = document.getElementById("guessBoards").appendChild(player2guess);
+    p2Guess.setAttribute("id", "p2Guess");
+    p2GuessArr = createArray(10, 9);
 
-    p2grid = document.getElementById("boards").appendChild(player2grid);
-    p2grid.setAttribute("id", "p2grid");
+    p2Grid = document.getElementById("boards").appendChild(player2grid);
+    p2Grid.setAttribute("id", "p2Grid");
+    p2GridArr = createArray(10, 9);
 
     gameOn = true;
     gameLoop();
@@ -204,18 +286,18 @@ function changeTurn()
         isChanging = 0;
         if(curPlyr == 1)
         {
-            p1grid.style.display = "inline";
-            p1guess.style.display = "inline";
-            p2grid.style.display = "none";
-            p2guess.style.display = "none";
+            p1Grid.style.display = "inline";
+            p1Guess.style.display = "inline";
+            p2Grid.style.display = "none";
+            p2Guess.style.display = "none";
             curPlyr++;
         }
         else
         {   
-            p1grid.style.display = "none";
-            p1guess.style.display = "none";
-            p2grid.style.display = "inline";
-            p2guess.style.display = "inline";
+            p1Grid.style.display = "none";
+            p1Guess.style.display = "none";
+            p2Grid.style.display = "inline";
+            p2Guess.style.display = "inline";
             curPlyr--;
         }
         document.getElementById("playerNum").innerHTML = curPlyr;
@@ -224,5 +306,13 @@ function changeTurn()
 
 function gameLoop()
 {
-    // TODO: Create game loop
+    while(gameOn)
+    {
+        p1Grid.style.display = "inline";
+        p1Guess.style.display = "inline";
+        p2Grid.style.display = "none";
+        p2Guess.style.display = "none";
+        alert("Player 1, select on bottom where you will place your first ship. Must be three long/wide, horizontal/vertical only, and must not overlap any other ship.");
+        gameOn = false;
+    }
 }
